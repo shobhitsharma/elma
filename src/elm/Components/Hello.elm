87bca887c77fd4e63a20bfc24d0821acc594569c
shobhitsharma <ishobhitsharma@gmail.com>
2016-10-18 00:35:22 +0200

    Switched to decent Elm structure

---
+++ src/elm/Components/Hello.elm
@@ -0,0 +1,12 @@
+module Components.Hello exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import String
+
+-- hello component
+hello : Int -> Html a
+hello model =
+  div
+    [ class "h1" ]
+    [ text ( "Ola, Elm" ++ ( "!" |> String.repeat model ) ) ]
